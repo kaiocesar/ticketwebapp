f22b2374dc80afeda77f2cf4c4e10be09d280c88
Kaio Cesar <tecnico.kaio@gmail.com>
2016-08-12 13:49:52 -0300

    validação basica de login concluida

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -27,9 +27,16 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('LoginCtrl', function($scope){
+.controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state){
   $scope.data = {};
   $scope.login = function(){
-    console.log("Fiz esse reggae para você");
+    LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data){
+      $state.go("tab.dash");
+    }).error(function(data){
+      var alertPopup = $ionicPopup.alert({
+        title: 'Login failed!',
+        template: 'Please check your credentials'
+      });
+    });
   };
 });
